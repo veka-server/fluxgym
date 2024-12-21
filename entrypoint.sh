2c1b819ece7b473730f612d31cb7820b137331c7
Veka <veka610@gmail.com>
2024-12-21 10:49:24 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,9 @@ set -e
 # Check if requirements.txt exists and install dependencies
 if [ -f "/app/fluxgym/requirements.txt" ]; then
     echo "Installing Python dependencies..."
-    pip install --no-cache-dir -r /app/sd-scripts/requirements.txt
+#    pip install --no-cache-dir -r /app/sd-scripts/requirements.txt
     pip install --no-cache-dir -r /app/fluxgym/requirements.txt
+    pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu122/torch_stable.html
 else
     echo "requirements.txt not found, skipping dependency installation."
 fi
