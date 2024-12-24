d3eeb0bbbf248ed1945bafe92673b290532b69a3
Veka <veka610@gmail.com>
2024-12-24 10:25:04 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,10 @@
 # Ensure Hugging Face models are downloaded if HF_HUB_OFFLINE is set to 0
 if [ "$HF_HUB_OFFLINE" = "1" ]; then
     echo "Downloading Hugging Face models..."
-    huggingface-cli download openai/clip-vit-large-patch14
-    huggingface-cli download google/t5-v1_1-xxl
-    huggingface-cli download MiaoshouAI/Florence-2-base-PromptGen-v2.0
+    HF_HUB_OFFLINE=0 huggingface-cli download openai/clip-vit-large-patch14
+    HF_HUB_OFFLINE=0 huggingface-cli download google/t5-v1_1-xxl
+    HF_HUB_OFFLINE=0 huggingface-cli download MiaoshouAI/Florence-2-base-PromptGen-v2.0
+    HF_HUB_OFFLINE=1
 else
     echo "HF_HUB_OFFLINE is set to 0, skipping model downloads."
 fi
