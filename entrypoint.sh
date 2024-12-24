669258b5a63bd7c132bb21b9a018f516cad2ac28
Veka <veka610@gmail.com>
2024-12-24 15:55:37 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,17 @@ if [ "$HF_HUB_OFFLINE" = "1" ]; then
 #    HF_HUB_OFFLINE=0 huggingface-cli download google/t5-v1_1-xxl t5xxl_fp16.safetensors
     HF_HUB_OFFLINE=0 huggingface-cli download MiaoshouAI/Florence-2-base-PromptGen-v2.0
     
-    HF_HUB_OFFLINE=0 huggingface-cli download cocktailpeanut/xulf-dev flux1-dev.sft --local-dir models/unet
-    HF_HUB_OFFLINE=0 huggingface-cli download cocktailpeanut/xulf-dev ae.sft --local-dir models/vae
-    HF_HUB_OFFLINE=0 huggingface-cli download comfyanonymous/flux_text_encoders clip_l.safetensors --local-dir models/clip
-    HF_HUB_OFFLINE=0 huggingface-cli download comfyanonymous/flux_text_encoders t5xxl_fp16.safetensors --local-dir models/clip
-    HF_HUB_OFFLINE=0 huggingface-cli download comfyanonymous/flux_text_encoders clip_l.safetensors --local-dir models/clip
+#    HF_HUB_OFFLINE=0 huggingface-cli download cocktailpeanut/xulf-dev flux1-dev.sft --local-dir models/unet
+#    HF_HUB_OFFLINE=0 huggingface-cli download cocktailpeanut/xulf-dev ae.sft --local-dir models/vae
+#    HF_HUB_OFFLINE=0 huggingface-cli download comfyanonymous/flux_text_encoders clip_l.safetensors --local-dir models/clip
+#    HF_HUB_OFFLINE=0 huggingface-cli download comfyanonymous/flux_text_encoders t5xxl_fp16.safetensors --local-dir models/clip
+#    HF_HUB_OFFLINE=0 huggingface-cli download comfyanonymous/flux_text_encoders clip_l.safetensors --local-dir models/clip
+    
+    HF_HUB_OFFLINE=0 huggingface-cli download cocktailpeanut/xulf-dev flux1-dev.sft 
+    HF_HUB_OFFLINE=0 huggingface-cli download cocktailpeanut/xulf-dev ae.sft 
+    HF_HUB_OFFLINE=0 huggingface-cli download comfyanonymous/flux_text_encoders clip_l.safetensors 
+    HF_HUB_OFFLINE=0 huggingface-cli download comfyanonymous/flux_text_encoders t5xxl_fp16.safetensors 
+    HF_HUB_OFFLINE=0 huggingface-cli download comfyanonymous/flux_text_encoders clip_l.safetensors 
 
     HF_HUB_OFFLINE=1
 else
